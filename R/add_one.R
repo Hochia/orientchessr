bfc55a00172be49fec4d4737234491fd9d08229d
Hochia <johnny30618@gmail.com>
2020-03-25 03:41:43 +0800

    Add add_one()

---
+++ R/add_one.R
@@ -0,0 +1,35 @@
+#' Add one or more pieces to the chessboard
+#'
+#' @param data list
+#' @param name character, name of the chess
+#' @param x numeric, x-axis
+#' @param y numeric, y-axis
+#'
+#' @return Add pieces to the data
+#' @importFrom magrittr %>%
+#' @export
+#'
+#' @examples
+#' initiate_chessboard() %>%
+#'   add_one("MA", 5, 1)
+#'
+#' pname <- c("BING", "ZU", "SHI", "SHI", "XIANG", "PAO")
+#' px <- c(1,1,4,6,3,2)
+#' py <- c(4,7,1,1,1,3)
+#'
+#' initiate_chessboard() %>%
+#'   add_one(pname, px, py)
+add_one <- function(data, name, x, y) {
+
+  if (!is.data.frame(data)) {
+    df <- data[[length(data)]][[1]]
+  }
+  else {
+    df <- data
+  }
+
+  df <- df %>%
+    dplyr::add_row(name = name, x = x, y = y) %>%
+    dplyr::filter(name != "name")
+  df
+}
