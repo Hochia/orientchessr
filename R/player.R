f2a35f7f72924df51fdbb41c749b6e2dd3059d71
Hochia <johnny30618@gmail.com>
2020-04-24 16:36:28 +0800

    General interface for game and puzzle

--- R/player.R
+++ R/player.R
@@ -10,48 +10,40 @@
 #'
 #' @examples
 #' race_start() %>%
-#' player(c(2,1), c(3, 3)) %>%
-#' player(c(8,8), c(5, 8)) %>%
-#' player(c(2,3), c(2, 10))
+#'   player(c(2,1), c(3, 3)) %>%
+#'   player(c(8,8), c(5, 8)) %>%
+#'   player(c(2,3), c(2, 10))
 player <- function(data, before, after) {
 
-  bf <- before
-  af <- after
+  bf <- paste0(before, collapse = ",")
+  af <- paste0(after, collapse = ",")
 
-  if (ncol(data[[2]]) == 2) {
-    data[[1]] <- data[[1]] %>%
-      dplyr::add_row(name = dplyr::filter(data[[2]], at == paste0(bf, collapse = ","))$name,
-                     before = paste0(bf, collapse = ","),
-                     after = paste0(af, collapse = ","))
+  df <- data[[2]] %>%
+    dplyr::mutate(
+      state = dplyr::case_when(
+        (at == af & state == TRUE) | state == FALSE ~ FALSE,
+        TRUE ~ TRUE),
+      at = dplyr::case_when(
+        at == bf & state == TRUE ~ af,
+        TRUE ~ at),
+    ) %>%
+    dplyr::select(name, at, state)
 
-    df <- data[[2]] %>%
-      dplyr::mutate(before = at,
-                    after = dplyr::case_when(before == paste0(bf, collapse = ",") ~ paste0(af, collapse = ","),
-                                             TRUE ~ before),
-                    state = rep(TRUE, 32)) %>%
-      dplyr::select(-at)
-  } else {
-    data[[1]] <- data[[1]] %>%
-      dplyr::add_row(name = dplyr::filter(data[[2]], before == paste0(bf, collapse = ","))$name,
-                     before = paste0(bf, collapse = ","),
-                     after = paste0(af, collapse = ","))
+  data[[1]] <- data[[1]] %>%
+    dplyr::add_row(
+      name = dplyr::filter(df, at == af, state == TRUE)$name, # be carful to choose only one piece
+      before = bf,
+      after = af
+    )
 
-    df <- data[[2]] %>%
-      dplyr::mutate(state = dplyr::case_when(before == paste0(af, collapse = ",") ~ FALSE,
-                               TRUE ~ TRUE),
-                    before = after,
-                    after = dplyr::case_when(before == paste0(bf, collapse = ",") ~ paste0(af, collapse = ","),
-                                             TRUE ~ before)) %>%
-      dplyr::select(name, before, after, state)
-  }
-
-  xy <- unlist(stringr::str_split(df$after, ","))
+  xy <- unlist(stringr::str_split(df$at, ","))
 
   df2 <- df %>%
-    dplyr::mutate(p1 = dplyr::case_when(name %in% ccname ~ TRUE,
-                                        TRUE ~ FALSE),
-                  x = as.integer(xy[1:length(xy) %% 2 == 1]),
-                  y = as.integer(xy[1:length(xy) %% 2 == 0])) %>%
+    dplyr::mutate(
+      p1 = dplyr::case_when(name %in% ccname ~ TRUE, TRUE ~ FALSE),
+      x = as.integer(xy[1:length(xy) %% 2 == 1]),
+      y = as.integer(xy[1:length(xy) %% 2 == 0])
+    ) %>%
     dplyr::filter(state == TRUE)
 
   gf <- ggplot2::ggplot() +
